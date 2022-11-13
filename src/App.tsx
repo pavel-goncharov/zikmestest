1d06ca579a2e1bb12880e02d296be4a485c2e294
pavel-goncharov <pawellgoncharov@gmail.com>
2022-11-13 22:52:23 +0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {FC} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowRight} from "@fortawesome/free-solid-svg-icons/faArrowRight";
-import '@/styles/styles.module.scss';
 
 const App: FC = () => {
   return (
